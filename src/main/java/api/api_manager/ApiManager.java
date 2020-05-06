2137582d66c32ad2939cec22bcfef669cb3e18d6
polev01 <jenia_lt@mail.ru>
2020-05-06 02:31:50 -0700

    my first commit

---
+++ src/main/java/api/api_manager/ApiManager.java
@@ -0,0 +1,1 @@
+package api.api_manager;import api.models.google_places.GooglePlacesModel;import lombok.Getter;import lombok.Setter;public class ApiManager {    @Getter    @Setter    GooglePlacesModel googlePlacesModel = new GooglePlacesModel();}
---
+++ src/test/java/base/BaseTest.java
@@ -0,0 +1,1 @@
+package base;import api.api_manager.ApiManager;public class BaseTest {    protected ApiManager apiManager = new ApiManager();}
